00e12402b9ccbcdb017a42d99334606551b69462
nelonm <acsigy@gmail.com>
2024-02-17 12:18:54 +0100

    made backend, server, frontend

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello world")
+import express from "express"
+import {z} from "zod"
+import cors from "cors"
+import fs from "fs/promises"
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+
+
+
+
+
+
+
+
+
+
+server.listen(4100)
